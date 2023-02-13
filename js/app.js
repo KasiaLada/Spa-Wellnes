e1bb6e21c0832820199941be726b38556d9d82af
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2023-02-13 22:25:33 +0100

    funkcjonalnosci js

--- js/app.js
+++ js/app.js
@@ -31,4 +31,69 @@ document.addEventListener("DOMContentLoaded", function () {
   for (let i = 0; i < readMoreLessBtns.length; i++) {
     readMoreLessBtns[i].addEventListener("click", showHideText);
   }
+
+  //   newsletter
+  const input = document.querySelector(".newsletter-form input");
+  const newsletterBtn = document.querySelector(".newsletter-form button");
+  const formInfo = document.querySelector(".form-info");
+
+  newsletterBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    formInfo.style.display = "block";
+
+    if (input.value !== "") {
+      formInfo.textContent = "Zapisano do newslettera";
+      input.value = "";
+    } else {
+      formInfo.textContent = "Pole nie może byc puste, podaj aders email ";
+    }
+  });
+
+  // slider
+  const slider = document.querySelector(".slider");
+  const sliderStage = document.querySelector(".slider-stage");
+  const slides = document.querySelectorAll(".slider li");
+  const prev = document.querySelector(".previous-arrow");
+  const next = document.querySelector(".next-arrow");
+
+  const slideWidth = slides[0].clientWidth;
+  let currentIndex = 0;
+  let slidesNumber = slides.length - 1;
+
+  function goToSlide(index) {
+    
+    if (index < 0) {
+      index = slidesNumber;
+    } else if (index > slidesNumber) {
+      index = 0;
+    }
+
+    slider.style.left = index * (-slideWidth) + "px";
+    currentIndex = index;
+  }
+
+  function slideToNext() {
+    goToSlide(currentIndex + 1);
+  }
+  function slideToPrev() {
+    goToSlide(currentIndex - 1);
+  }
+
+  prev.addEventListener("click", slideToPrev);
+  next.addEventListener("click", slideToNext);
+  setInterval(slideToNext, 4000);
+
+
+
+//     go-up button
+  
+  const goUpBtn = document.querySelector(".go-up i");
+
+  goUpBtn.addEventListener("click", function () {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior:"smooth"
+    })
+  })
 });
